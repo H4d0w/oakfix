f6c256a7223f5c327f1c038aa22ff3cce230705c
H4d0w <134791593+H4d0w@users.noreply.github.com>
2023-12-23 14:51:52 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,6 +14,7 @@ readonly linux_pkgs=(
 
 readonly ubuntu_pkgs=(
     ${linux_pkgs[@]}
+    # https://docs.opencv.org/master/d7/d9f/tutorial_linux_install.html
     build-essential
     libgtk2.0-dev
     pkg-config
@@ -21,15 +22,16 @@ readonly ubuntu_pkgs=(
     libavformat-dev
     libswscale-dev
     python3-dev
-    libtbb12
+    libtbb2
     libtbb-dev
     libjpeg-dev
     libpng-dev
     libtiff-dev
+    # https://stackoverflow.com/questions/55313610
     ffmpeg
     libsm6
     libxext6
-    libgl1-mesa-dri
+    libgl1-mesa-glx
     python3-pyqt5
     python3-pyqt5.qtquick
     qml-module-qtquick-controls2
@@ -70,12 +72,15 @@ readonly ubuntu_arm_pkgs=(
 readonly fedora_pkgs=(
     ${linux_pkgs[@]}
     gtk2-devel
+    # Fedora uses pkgconf instead of pkg-config
     tbb-devel
     libjpeg-turbo-devel
     libpng-devel
     libtiff-devel
     libdc1394-devel
-    # Additional packages for Fedora may be added here
+    # TODO(PM): ffmpeg requires enabling rpmfusion-free-updates
+    # TODO(PM): libavcodec-dev libavformat-dev libswscale-dev python-dev libtbb2
+    #           libsm6 libxext6 libgl1-mesa-glx
 )
 
 print_action () {
@@ -89,18 +94,70 @@ print_and_exec () {
 }
 
 if [[ $(uname) == "Darwin" ]]; then
-    # ... [Remainder of the script for Darwin]
+    echo "During Homebrew install, certain commands need 'sudo'. Requesting access..."
+    sudo true
+    homebrew_install_url="https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
+    print_action "Installing Homebrew from $homebrew_install_url"
+    # CI=1 will skip some interactive prompts
+    CI=1 /bin/bash -c "$(curl -fsSL $homebrew_install_url)"
+    print_and_exec brew install git
+    echo
+    echo "=== Installed successfully!  IMPORTANT: For changes to take effect,"
+    echo "please close and reopen the terminal window, or run:  exec \$SHELL"
 elif [ -f /etc/os-release ]; then
+    # shellcheck source=/etc/os-release
     source /etc/os-release
+
     if [[ "$ID" == "ubuntu" || "$ID" == "debian" || "$ID_LIKE" == "ubuntu" || "$ID_LIKE" == "debian" || "$ID_LIKE" == "ubuntu debian" ]]; then
-        # ... [Ubuntu/Debian specific installation commands]
+        if [[ ! $(uname -m) =~ ^arm* ]]; then
+            sudo apt-get update
+            if [[ "$VERSION_ID" > "22.04" || "$VERSION_ID" == "22.04" ]]; then
+                sudo apt-get install -y "${ubuntu_pkgs_post22_04[@]}"
+            else
+                sudo apt-get install -y "${ubuntu_pkgs_pre22_04[@]}"
+            fi
+            python3 -m pip install --upgrade pip
+        elif [[ $(uname -m) =~ ^arm* ]]; then
+            sudo apt-get update
+            sudo apt-get install -y "${ubuntu_arm_pkgs[@]}"
+            python3 -m pip install --upgrade pip
+        fi
+
+        # As set -e is set, retrieve the return value without exiting
+        RET=0
+        dpkg -s uvcdynctrl > /dev/null 2>&1 || RET=$? || true
+        # is uvcdynctrl installed
+        if [[ "$RET" == "0" ]]; then
+          echo -e "\033[33mWe detected \"uvcdynctrl\" installed on your system. \033[0m"
+          echo -e "\033[33mWe recommend removing this package, as it creates a huge log files if a camera is used in UVC mode (webcam)\033[0m"
+          echo -e "\033[33mYou can do so by running the following commands:\033[0m"
+          echo -e "\033[33m$ sudo apt remove uvcdynctrl uvcdynctrl-data\033[0m"
+          echo -e "\033[33m$ sudo rm -f /var/log/uvcdynctrl-udev.log\033[0m"
+          echo ""
+        fi
+
+        OS_VERSION=$(lsb_release -r |cut -f2)
+        if [ "$OS_VERSION" == "21.04" ]; then
+            echo -e "\033[33mThere are known issues with running our demo script on Ubuntu 21.04, due to package \"python3-pyqt5.sip\" not being in a correct version (>=12.9)\033[0m"
+            echo -e "\033[33mWe recommend installing the updated version manually using the following commands\033[0m"
+            echo -e "\033[33m$ wget http://mirrors.kernel.org/ubuntu/pool/universe/p/pyqt5-sip/python3-pyqt5.sip_12.9.0-1_amd64.deb\033[0m"
+            echo -e "\033[33m$ sudo dpkg -i python3-pyqt5.sip_12.9.0-1_amd64.deb\033[0m"
+            echo ""
+        fi
     elif [[ "$ID" == "fedora" ]]; then
-        # ... [Fedora specific installation commands]
+        sudo dnf update -y
+        sudo dnf install -y "${fedora_pkgs[@]}"
+        sudo dnf groupinstall -y "Development Tools" "Development Libraries"
+        python3 -m pip install --upgrade pip
     else
         echo "ERROR: Distribution not supported"
         exit 99
     fi
-    # ... [Post-installation commands]
+
+    # Allow all users to read and write to Myriad X devices
+    echo "Installing udev rules..."
+    echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules > /dev/null
+    sudo udevadm control --reload-rules && sudo udevadm trigger
 else
     echo "ERROR: Host not supported"
     exit 99
